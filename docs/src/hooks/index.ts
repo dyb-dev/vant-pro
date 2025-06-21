8a63d25780aa5489ab9295f31134b846adcd0029
dyb-dev <2737993954@qq.com>
2025-06-21 14:35:32 +0800

    ✨ feat: 新增 styles mixin 和 hooks 函数

---
+++ docs/src/hooks/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: dyb-dev
+ * @Date: 2025-06-21 14:09:05
+ * @LastEditors: dyb-dev
+ * @LastEditTime: 2025-06-21 14:18:33
+ * @FilePath: /vant-pro/docs/src/hooks/index.ts
+ * @Description: hook模块
+ */
+
+/** 导出滚动控制相关hooks函数 */
+export * from "./scroll"
