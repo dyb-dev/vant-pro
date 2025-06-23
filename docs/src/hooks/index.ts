9744a779651227f07799b63497ac0d963a9e3a2a
dyb-dev <2737993954@qq.com>
2025-06-24 00:32:09 +0800

    ✨ feat: 新增interaction相关hooks

--- docs/src/hooks/index.ts
+++ docs/src/hooks/index.ts
@@ -2,10 +2,12 @@
  * @Author: dyb-dev
  * @Date: 2025-06-21 14:09:05
  * @LastEditors: dyb-dev
- * @LastEditTime: 2025-06-21 14:18:33
+ * @LastEditTime: 2025-06-24 00:27:44
  * @FilePath: /vant-pro/docs/src/hooks/index.ts
  * @Description: hook模块
  */
 
+/** 导出用户交互相关hooks函数 */
+export * from "./interaction"
 /** 导出滚动控制相关hooks函数 */
 export * from "./scroll"
